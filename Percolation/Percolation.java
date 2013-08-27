378debc80bfde3f34760eaa9e5233584812a3394
Daniel Chen <dychen@caltech.edu>
2013-08-26 22:44:47 -0700

    Initial commit. Did most of Percolation.

---
+++ Percolation/Percolation.java
@@ -0,0 +1,134 @@
+import java.util.*;
+import java.lang.IndexOutOfBoundsException;
+import java.lang.IllegalArgumentException;
+
+public class Percolation {
+
+    private int size;
+    public ArrayList<Integer> blocked;
+    private QuickFindUF qfalg;
+
+    /* 
+     * Constructor. Initializes an NxN grid of blocked cells.
+     * Input N: The size of the grid.
+     */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("Grid size must be positive.");
+        }
+
+        size = N;
+        blocked = new ArrayList<Integer>();
+        // Initialize the grid of blocked cells.
+        for (int i = 0; i < N * N; i++) {
+            blocked.add(i);
+        }
+
+        // Initialize the QF representation of these cells.
+        // The first N * N cells represent the corresponding cells in the grid.
+        // The N * N cell is the top node and should be connected to each 
+        // cell in the top row.
+        // The N * N + 1 cell is the bottom node and should be connected to each
+        // cell in the bottom row.
+        qfalg = new QuickFindUF(N * N + 2);
+    }
+
+    /*
+     * Opens the cell at row i, column j.
+     * Input i: The row of the cell (from 1 to N).
+     * Input j: The column of the cell (from 1 to N).
+     */
+    public void open(int i, int j) {
+        if (i < 1 || i > this.size || j < 1 || j > this.size) {
+            throw new IndexOutOfBoundsException("Indices must be from 1 to N");
+        }
+
+        int rmod = i-1;
+        int cmod = j-1;
+        int mod_index = rmod * size + cmod;
+        // Connect to left cell
+        if (j > 1 && isOpen(i, j-1))
+            qfalg.union(mod_index, rmod * size + (cmod-1));
+        // Connect to right cell
+        if (j < size && isOpen(i, j+1))
+            qfalg.union(mod_index, rmod * size + (cmod+1));
+        // Connect to upper cell
+        if (i > 1 && isOpen(i-1, j))
+            qfalg.union(mod_index, (rmod-1) * size + cmod);
+        // Connect to lower cell
+        if (i < size && isOpen(i+1, j))
+            qfalg.union(mod_index, (rmod+1) * size + cmod);
+        // If it's on the top row, connect it to the top cell.
+        if (i == 1) {
+            qfalg.union(mod_index, size * size);
+        }
+        // If it's on the bottom row, connect it to the bottom cell.
+        if (i == size) {
+            qfalg.union(mod_index, size * size + 1);
+        }
+    }
+
+    /*
+     * Returns whether or not the cell at (i, j) is open.
+     * Input i: The row of the cell (from 1 to N).
+     * Input j: The column of the cell (from 1 to N).
+     * Output: Whether or not the cell is open.
+     */
+    public boolean isOpen(int i, int j) {
+        if (i < 1 || i > size || j < 1 || j > size) {
+            throw new IndexOutOfBoundsException("Indices must be from 1 to N");
+        }
+
+        int rmod = i-1;
+        int cmod = j-1;
+        int mod_index = rmod * size + cmod;
+        return !blocked.contains(mod_index);
+    }
+
+    /*
+     * Returns whether or not there exists a percolation from the cell at (i, j)
+     * to the top of the grid. That is, it returns true if the cell at (i, j) is
+     * in the same set as any open cell in the top row of the grid.
+     * Input i: The row of the cell (from 1 to N).
+     * Input j: The column of the cell (from 1 to N).
+     * Output: Whether or not the system percolates to the input cell.
+     */
+    public boolean isFull(int i, int j) {
+        if (i < 1 || i > size || j < 1 || j > size) {
+            throw new IndexOutOfBoundsException("Indices must be from 1 to N");
+        }
+
+        int rmod = i-1;
+        int cmod = j-1;
+        int mod_index = rmod * size + cmod;
+        // Return whether or not the input node is connected to the top node.
+        return qfalg.connected(mod_index, size * size);
+    }
+
+    /*
+     * Returns whether or not the system percolates. That is, it returns true if
+     * there exists some cell in the bottom row that is in the same set as any 
+     * open cell in the top row of the grid.
+     * Output: Whether or not the system percolates.
+     */
+    public boolean percolates() {
+        // Return whether or not the bottom node is connected to the top node.
+        return qfalg.connected(size * size, size * size + 1);
+    }
+
+    /*
+     * Chooses a random open cell and opens it.
+     */
+    public void openRandom() {
+        //StdOut.println(blocked);
+        //StdIn.readInt();
+        double rand = StdRandom.random();
+        int blockedIdx = (int) Math.floor(rand * blocked.size());
+        int idx = blocked.remove(blockedIdx);
+        //StdOut.printf("Size: %d\n", blocked.size());
+        //StdOut.printf("Removed: %d\n", blockedIdx);
+        int row = idx / size + 1; // + 1 because open() params are
+        int col = idx % size + 1; // 1-indexed
+        open(row, col);
+    }
+}
---
+++ Percolation/PercolationStats.java
@@ -0,0 +1,94 @@
+import java.lang.IllegalArgumentException;
+
+public class PercolationStats {
+    private double mean;
+    private double stddev;
+    private double confidenceLo;
+    private double confidenceHi;
+
+    /*
+     * Constructor. Performs T percolation experiments on NxN grids.
+     * Input N: The size of the grid.
+     * Input T: The number of trials.
+     */
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Inputs must be positive.");
+        }
+
+        double[] numOpenSites = new double[T];
+        int counts;
+        int openCellIdx;
+        int row;
+        int col;
+        // Perform percolation experiments
+        for (int i = 0; i < T; i++) {
+            StdOut.printf("Trial # %d\n", i+1);
+            Percolation p = new Percolation(N);
+            counts = 0;
+            while (!p.percolates()) {
+                p.openRandom();
+                counts++;
+            }
+            StdOut.println(counts);
+            numOpenSites[i] = ((double) counts) / (N * N);
+        }
+        // Calculate statistics.
+        mean = StdStats.mean(numOpenSites);
+        stddev = StdStats.stddev(numOpenSites);
+        confidenceLo = mean - 1.96 * stddev / Math.sqrt(T);
+        confidenceHi = mean + 1.96 * stddev / Math.sqrt(T);
+    }
+
+    /*
+     * Returns the sample mean from the percolation experiments.
+     * Output: Sample mean.
+     */
+    public double mean() {
+        return mean;
+    }
+
+    /*
+     * Returns the same standard deviation from the percolation experiments.
+     * Output: Sample standard deviation.
+     */
+    public double stddev() {
+        return stddev;
+    }
+
+    /*
+     * Returns the lower bound of the 95% CI from the percolation experiments.
+     * Output: Lower bound of the confidence interval.
+     */
+    public double confidenceLo() {
+        return confidenceLo;
+    }
+
+    /*
+     * Returns the upper bound of the 95% CI from the percolation experiments.
+     * Output: Upper bound of the confidence interval.
+     */
+    public double confidenceHi() {
+        return confidenceHi;
+    }
+
+    /*
+     * Main method. Takes two command-line arguments, N and T, which should be 
+     * positive integers.
+     * Input args: Two positive integers (N T) from the command line 
+     * (e.g. 400 100)
+     */
+    public static void main(String[] args) {
+        StdOut.println("Enter the grid dimension N " + 
+                           "(must be a positive integer): ");
+        int N = StdIn.readInt();
+        StdOut.println("Enter the number of trials T " + 
+                           "(must be a positive integer): ");
+        int T = StdIn.readInt();
+        PercolationStats ps = new PercolationStats(N, T);
+        StdOut.printf("mean                    = %f\n", ps.mean());
+        StdOut.printf("stddev                  = %f\n", ps.stddev());
+        StdOut.printf("95%% confidence interval = %f, %f\n", ps.confidenceLo(),
+                      ps.confidenceHi());
+    }
+}
